ecf73914faf6679ebeb5244855efece73b7c2f7c
calebegg <calebegg@0652ecd2-cd0d-4042-a743-22bcdc4d746b>
2011-09-13 03:27:49 +0000

    An attempt at (pow ...) and (rp ...) (Russian peasants algorithm). Termination for both works, but haven't gotten equivalence yet.

---
+++ not-working/exponentiation.lisp
@@ -0,0 +1,20 @@
+(include-book "doublecheck" :dir :teachpacks)
+(include-book "arithmetic-5/top" :dir :system)
+
+(defun pow (b n)
+  (if (zp n)
+      1
+      (* b (pow b (1- n)))))
+
+(defun rp (b n)
+  (cond ((zp n)
+         1)
+        ((oddp n)
+         (* b (rp (* b b) (floor n 2))))
+        ((evenp n)
+         (rp (* b b) (floor n 2)))))
+
+(defproperty rp-equivalent-to-pow
+  (b :value (random-natural)
+   n :value (random-natural))
+  (= (pow b n) (rp b n)))
