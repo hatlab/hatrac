98d41a1ac578ed68328308bb7236e32f43fc49c6
calebegg <calebegg@0652ecd2-cd0d-4042-a743-22bcdc4d746b>
2011-09-20 20:57:15 +0000

    String max function from proposition lectures. ACL2's string comparator is weird; I think writing a new one is the way to fix this.

---
+++ not-working/string-maximum.lisp
@@ -0,0 +1,28 @@
+(include-book "doublecheck" :dir :teachpacks)
+(include-book "str/top" :dir :system)
+
+(defun string-max (r s)
+  (if (integerp (string> r s))
+      r
+      s))
+
+(defun maximum (ss)
+  (if (endp (rest ss))
+      (first ss)
+      (string-max (first ss) (maximum (rest ss)))))
+
+(defun in (x xs)
+  (if (endp xs)
+      nil
+      (or (equal x (first xs)) (in x (rest xs)))))
+
+(defproperty maximum-greater-than-any-member-2
+  (ss :where (true-listp ss) :value (random-list-of (random-string) :size (1+ (random-data-size)))
+   s :where (in s ss) :value (random-element-of ss))
+  (integerp (string<= s (maximum ss))))
+
+;(defproperty maximum-greater-than-any-member
+;  (n :where (natp n) :value (random-data-size)
+;   ss :where (and (true-listp ss) (< (len ss) n)) :value (random-list-of (random-string) :size n)
+;   k :where (and (natp k) (< k n)) :value (random-between 0 (1- n)))
+;  (integerp (string<= (nth k ss) (maximum ss))))
