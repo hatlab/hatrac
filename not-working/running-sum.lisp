1ae7ca428bb979a5387c967dcc34c650fa9cd789
calebegg <calebegg@0652ecd2-cd0d-4042-a743-22bcdc4d746b>
2011-09-20 20:58:07 +0000

    sum and sumr equivalence. Not sure how to make it work.

---
+++ not-working/running-sum.lisp
@@ -0,0 +1,17 @@
+(include-book "doublecheck" :dir :teachpacks)
+(include-book "arithmetic-3/top" :dir :system)
+
+(defun sum (xs)
+  (if (endp xs)
+      0
+      (+ (first xs) (sum (rest xs)))))
+
+(defun sumr (s xs)
+  (if (endp xs)
+      s
+      (sumr (+ s (first xs)) (rest xs))))
+
+(defproperty sumr-equals-sum
+  (xs :where (true-listp xs) :value (random-list-of (random-rational))
+   s :where (rationalp s) :value (random-rational))
+  (equal (sumr s xs) (+ s (sum xs))))
