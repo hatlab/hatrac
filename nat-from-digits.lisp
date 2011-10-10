c97e1997316b1a8cb87e2003a59c7b6db0e680ef
calebegg <calebegg@0652ecd2-cd0d-4042-a743-22bcdc4d746b>
2011-10-10 17:23:30 +0000

    More work on nats and numerals. Several thms don't admit.

--- nat-from-digits.lisp
+++ nat-from-digits.lisp
@@ -28,10 +28,63 @@
         (and (integerp d) (>= d 0) (< d b) 
              (valid-digitsp b (rest ds))))))
 
-(defproperty horners-rule-works
-  (b :where (not (zp b))
-     :value (random-natural)
-   ds :where (valid-digitsp b ds)
-      :value (random-list-of (random-between 0 (1- b))))
-  (= (nat-from-digits b ds)
-     (horners-rule b ds)))
+;(defproperty horners-rule-works
+;  (b :where (not (zp b))
+;     :value (random-natural)
+;   ds :where (valid-digitsp b ds)
+;      :value (random-list-of (random-between 0 (1- b))))
+;  (= (nat-from-digits b ds)
+;     (horners-rule b ds)))
+
+(defun num (ds) (horners-rule 2 ds))
+
+(defun bits (n)
+  (if (and (integerp n) (> n 0))
+      (cons (mod n 2)
+            (bits (floor n 2)))
+      nil))
+
+(defproperty num-inverts-bits
+  (n :where (natp n) :value (random-natural))
+  (= (num (bits n)) n))
+
+(defun all-bitsp (ds)
+  (if (endp ds)
+      t
+      (and (or (= (first ds) 0)
+               (= (first ds) 1))
+           (all-bitsp (rest ds)))))
+
+(defproperty bits-reutrns-all-bits
+  (n :where (natp n) :value (random-natural))
+  (all-bitsp (bits n)))
+
+;(defproperty len-bits<=
+;  (w :where (and (integerp w) (> w 0)) :value (random-data-size)
+;   n :where (and (< n (pow 2 w))
+;                 (natp n))
+;     :value (random-natural))
+;  (<= (len (bits n)) w))
+;
+;(defproperty len-bits
+;  (w :where (natp w) :value (+ 1 (random-data-size))
+;   n :where (and (<= (pow 2 (- w 1)) n)
+;                 (< n (pow 2 w))
+;                 (natp n))
+;     :value (random-natural))
+;  (= (len (bits n)) w))
+
+(defun fin (xs)
+  (if (endp (rest xs))
+      (first xs)
+      (fin (rest xs))))
+
+(defproperty higest-bit-is-1
+  (n :where (and (natp n) (> n 0)) :value (+ 1 (random-natural)))
+  (= (fin (bits n)) 1))
+
+;; honors s 11 > Lecture 20 > Slide 2
+;; pad
+;; len-pad-equals-w
+;; prefix
+;; mod
