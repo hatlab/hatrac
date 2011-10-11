7e5713596fd5f88913754ab87a3389054f1e1ad4
calebegg <calebegg@0652ecd2-cd0d-4042-a743-22bcdc4d746b>
2011-10-11 22:02:40 +0000

    The customizations affect Dracula too, so I'm removing an unhelpful one.

--- acl2-customization.lisp
+++ acl2-customization.lisp
@@ -1,4 +1,4 @@
 (add-include-book-dir :teachpacks "teachpacks")
 
 ;;; '(error warning warning! observation prove proof-checker event expansion proof-tree summary))
-(set-inhibit-output-lst '(warning warning! observation prove proof-checker event expansion proof-tree summary))
+;(set-inhibit-output-lst '(warning warning! observation prove proof-checker event expansion proof-tree summary))
